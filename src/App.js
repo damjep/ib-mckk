739d5df3f18fa0243b585c7b82a31031a598fecc
damjep <adamjeff143@gmail.com>
2024-06-12 00:54:04 +0100

    adam

--- src/App.js
+++ src/App.js
@@ -24,6 +24,7 @@ import { Activities } from './Components/StudentCorner/Activities/Activities';
 import { Achievement } from './Components/StudentCorner/Achievements/Achievment';
 import { Graduation } from './Components/StudentCorner/Graduation/Graduation';
 
+
 function App() {
   return (<>
     <div className="App">
